1f426e50ac013ad23b9ce4c4238c93720e204310
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-09-02 12:53:52 +0100

    created dashboard

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { DollarSign, House, Info, Mail, Settings, ShoppingBag, ShoppingCart, Users } from 'lucide-react'
+import { DollarSign, House, Info, Mail, Settings, ShoppingBag, ShoppingCart, Users, Bell, Menu } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
 
 const ICONS = {
     House,
@@ -16,16 +19,39 @@ const ICONS = {
 }
 
 const Sidebar = () => {
+    const [isSidebarOpen,setIssidebarOpen] = useState(true)
 
     const [sidebarItems, setSidebarItems] = useState([])
     const pathname = usePathname()
 
     useEffect(() => {
-        fetch("data/data.json").then((res) => res.json()).then((data) => setSidebarItems(data.sidebarItems))
+        fetch("/data/data.json").then((res) => res.json()).then((data) => setSidebarItems(data.sidebarItems))
     }, [])
   return (
-    <div>
-      sidebar
+    <div className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${isSidebarOpen ? 'w-64' : 'w-20' }`}>
+       <div className='h-full bg-[#1e1e1e] backdrop-blur-md p-4 flex flex-col border-r border-[#2f2f2f]'>
+
+        <button onClick={() => setIssidebarOpen(!isSidebarOpen)} className='p-2 rounded-full hover:bg-[#2f2f2f] transition-colors max-w-fit cursor-pointer'>
+            <Menu size={24}/>
+        </button>
+        <nav className='mt-8 flex-grow'>
+            {sidebarItems.map((item) => {
+                const IconComponent = ICONS[item.icon]
+                return (
+                    <Link key={item.name} href={item.href}>
+                        <div className={`flex items-center p-4 text-sm font-medium rounded-lg hover:bg-[#2f2f2f] transition-colors mb-2 
+                        ${pathname === item.href ? "bg-[#2f2f2f]" : ""}`}>
+                            <IconComponent size={20} style={{ minWidth: "20px" }}/>
+
+                           {isSidebarOpen && (
+                            <span className='ml-4 whitespace-nowrap'>{item.name}</span>
+                           )}
+                        </div>
+                    </Link>
+                )
+            })}
+        </nav>
+       </div>
     </div>
   )
 }
